cbf03da0e8ef2cce6ecd986aa78155952a874c53
Vassilis Panos <4130346+vassilis-panos@users.noreply.github.com>
2021-02-10 11:40:03 +0200

    Add version key to manifest file

--- custom_components/smartir/__init__.py
+++ custom_components/smartir/__init__.py
@@ -19,7 +19,7 @@ from homeassistant.helpers.typing import ConfigType
 _LOGGER = logging.getLogger(__name__)
 
 DOMAIN = 'smartir'
-VERSION = '1.16.0'
+VERSION = '1.16.1'
 MANIFEST_URL = (
     "https://raw.githubusercontent.com/"
     "smartHomeHub/SmartIR/{}/"
