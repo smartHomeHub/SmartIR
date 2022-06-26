6a370b5e617dce4fa2486b15827e61588f28905f
Riceball LEE <snowyu.lee@gmail.com>
2022-06-26 13:22:07 +0800

    fix: forget to add the MQTT controller

--- custom_components/smartir/controllers/__init__.py
+++ custom_components/smartir/controllers/__init__.py
@@ -1,5 +1,6 @@
 from .abstract_controller import *
 from .broadlink_controller import *
+from .mqtt_controller import *
 from .lookin_controller import *
 from .esphome_controller import *
 from .xiaomi_controller import *
